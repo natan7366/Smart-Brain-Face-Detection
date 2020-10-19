eb4bafe2185b1bb796e2f6fbf5cdd235c7b12a5a
natan7366 <natan7366@gmail.com>
2020-10-19 16:53:16 +0300

    Updates

---
+++ precache-manifest.7681a0207b5280ddb417b9d10a93f12c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7041859a4099ec90e3bea8d2589e1b",
+    "url": "/Smart-Brain-Face-Detection/index.html"
+  },
+  {
+    "revision": "ac11239e8184105db13c",
+    "url": "/Smart-Brain-Face-Detection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b99e1a8b42e2d79937df",
+    "url": "/Smart-Brain-Face-Detection/static/css/main.8f093d78.chunk.css"
+  },
+  {
+    "revision": "ac11239e8184105db13c",
+    "url": "/Smart-Brain-Face-Detection/static/js/2.4325e6b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Smart-Brain-Face-Detection/static/js/2.4325e6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99e1a8b42e2d79937df",
+    "url": "/Smart-Brain-Face-Detection/static/js/main.399cc496.chunk.js"
+  },
+  {
+    "revision": "4a7b947fbd3d51a4876b",
+    "url": "/Smart-Brain-Face-Detection/static/js/runtime-main.77df7740.js"
+  }
+]);
